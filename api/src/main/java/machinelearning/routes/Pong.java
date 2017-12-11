69536cadd36daa2e957ab83682a92ca7f6616d18
beppek <beppe@live.se>
2017-12-11 18:29:25 +0100

    init api

---
+++ api/src/main/java/machinelearning/routes/Pong.java
@@ -0,0 +1,16 @@
+package machinelearning.routes;
+
+/**
+ * Pong response for ping controller
+ * */
+public class Pong {
+    private final String response;
+
+    public Pong() {
+        this.response = "Pong";
+    }
+
+    public String getContent() {
+        return response;
+    }
+}
