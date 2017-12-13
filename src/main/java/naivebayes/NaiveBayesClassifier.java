76dc84f4fcb560d659656f0b9920ffaef67af086
beppek <beppe@live.se>
2017-12-13 16:21:51 +0100

    better way to discard classvalues from table

--- src/main/java/naivebayes/NaiveBayesClassifier.java
+++ src/main/java/naivebayes/NaiveBayesClassifier.java
@@ -26,7 +26,7 @@ public class NaiveBayesClassifier implements Classifier {
         }
 
         //Get attributes
-        for (int i = 0; i < train.noAttributes(); i++) {
+        for (int i = 0; i < train.noAttributes() - 1; i++) {
             frequencyTable.put(train.getAttributeName(i), new HashMap<>());
         }
 
@@ -38,16 +38,12 @@ public class NaiveBayesClassifier implements Classifier {
             //Increase total count for classValues
             classValues.put(classValue, classValues.get(classValue) + 1);
             ArrayList<Attribute> attributes = instance.getAttributes();
-            for (int j = 0; j < instance.noAttributes(); j++) {
+            for (int j = 0; j < instance.noAttributes() - 1; j++) {
                 String nv = attributes.get(j).nominalValue();
                 String name = instance.getAttributeName(j);
                 //Increase count for values based on classValues
                 Map<String, Map<String, Double>> valueFrequency = frequencyTable.get(name);
                 Map<String, Double> classCountForAttribute;
-                //No need to store classValues
-                if (classValues.containsKey(nv)) {
-                    break;
-                }
                 //Build frequency table from ground up
                 if (valueFrequency.containsKey(nv)) {
                     classCountForAttribute = valueFrequency.get(nv);
@@ -83,15 +79,13 @@ public class NaiveBayesClassifier implements Classifier {
             Double pClass = classCount / instanceCount;
 
             probabilities.put(classValue, pClass);
-            for (int i = 0; i < inst.noAttributes(); i++) {
+            for (int i = 0; i < inst.noAttributes() - 1; i++) {
                 Attribute attribute = inst.getAttribute(i);
                 String name = inst.getAttributeName(i);
                 String value = attribute.nominalValue();
-                if (!classValues.containsKey(value)) {
-                    Double count = frequencyTable.get(name).get(value).get(classValue);
-                    Double pAttribute = count / classCount;
-                    probabilities.put(classValue, pAttribute * probabilities.get(classValue));
-                }
+                Double count = frequencyTable.get(name).get(value).get(classValue);
+                Double pAttribute = count / classCount;
+                probabilities.put(classValue, pAttribute * probabilities.get(classValue));
             }
         }
         String result = probabilities.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey();
