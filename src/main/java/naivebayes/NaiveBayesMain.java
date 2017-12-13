837a4973ea75ed71c5eb3772fb3bedb0e2c98c28
beppek <beppe@live.se>
2017-12-13 01:31:47 +0100

    runs the classifier

--- src/main/java/naivebayes/NaiveBayesMain.java
+++ src/main/java/naivebayes/NaiveBayesMain.java
@@ -1,9 +1,21 @@
 package naivebayes;
 
+import core.Evaluator;
+
+import java.io.File;
+
 public class NaiveBayesMain {
     public static void main(String[] args) {
+        //Train the weka implementation on the wiki set
         NaiveBayes wnb = new NaiveBayes("data/wikipedia_70.arff");
         wnb.train();
         wnb.test();
+
+        //Now train my implementation on the fifa set
+        File file = new File("data/FIFA_skill_nominal.arff");
+        String absolutePath = file.getAbsolutePath();
+        Evaluator evaluator = new Evaluator(new NaiveBayesClassifier(), absolutePath);
+        evaluator.evaluateWholeSet();
+        evaluator.evaluateCV();
     }
 }
