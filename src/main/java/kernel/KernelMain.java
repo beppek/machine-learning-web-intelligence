a0a00e68db71c65e3aa5848e93a0d433000bafc0
beppek <beppe@live.se>
2017-12-15 16:09:03 +0100

    kernel main

--- src/main/java/kernel/KernelMain.java
+++ src/main/java/kernel/KernelMain.java
@@ -1,4 +1,16 @@
 package kernel;
 
+import core.Evaluator;
+
+import java.io.File;
+
 public class KernelMain {
+    public static void main(String[] args) {
+        File file = new File("data/matchmaker_fixed.arff");
+        String absolutePath = file.getAbsolutePath();
+
+        Evaluator evaluator = new Evaluator(new KernelMethodClassifier("KernelMethodClassifier"), absolutePath);
+
+        evaluator.evaluateWholeSet();
+    }
 }
