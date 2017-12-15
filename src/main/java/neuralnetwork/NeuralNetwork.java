15263994269f87186b05e52a8d4fcde2ee512204
beppek <beppe@live.se>
2017-12-15 16:08:50 +0100

    neural network

--- src/main/java/neuralnetwork/NeuralNetwork.java
+++ src/main/java/neuralnetwork/NeuralNetwork.java
@@ -1,4 +1,36 @@
 package neuralnetwork;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+
 public class NeuralNetwork {
+    private String path;
+    private Instances data;
+    private Classifier cl;
+
+    NeuralNetwork(String path) throws Exception {
+        this.path = path;
+        readData();
+    }
+
+    public void train() throws Exception {
+        cl = new MultilayerPerceptron();
+        cl.buildClassifier(data);
+    }
+
+    public void test() throws Exception {
+        Evaluation eval = new Evaluation(data);
+        eval.crossValidateModel(cl, data, 10, new java.util.Random(1));
+        System.out.println(eval.toSummaryString());
+        System.out.println(eval.toMatrixString());
+    }
+
+    private void readData() throws Exception {
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource(path);
+        data = source.getDataSet();
+        data.setClassIndex(8);
+    }
 }
--- src/main/java/neuralnetwork/NeuralNetworkMain.java
+++ src/main/java/neuralnetwork/NeuralNetworkMain.java
@@ -1,4 +1,13 @@
 package neuralnetwork;
 
 public class NeuralNetworkMain {
+    public static void main(String[] args) {
+        try {
+            NeuralNetwork n = new NeuralNetwork("data/matchmaker_fixed.arff");
+            n.train();
+            n.test();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
